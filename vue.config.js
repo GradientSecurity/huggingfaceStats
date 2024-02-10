85e4f16f2bbf30598e2c075f2bfb51e48fca9c07
guynachshon <Guy.na8@gmail.com>
2024-02-10 20:18:02 +0200

    init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/huggingfaceStats/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/huggingfaceStats/' : '/',
+  title: 'Huggingface Stats',
+
 })
